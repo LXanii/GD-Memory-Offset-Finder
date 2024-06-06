2409d85a4d25cb2424c114a2b01966d22759c8bd
xanii <itzborgyyt@gmail.com>
2024-06-05 23:04:57 -0400

    im dumb

--- offset_finder.py
+++ offset_finder.py
@@ -5,4 +5,4 @@ min_address = 0x140000000 # change this according to ghidras min add during anal
 while True:
     address = input("\nMemory Address: ")
     print("Offset Address:", hex(int(address, 16) - min_address), "(Added to Clipboard)")
-    pyperclip.copy(hex(int(address, 16) - 0x140000000))
+    pyperclip.copy(hex(int(address, 16) - min_address))
